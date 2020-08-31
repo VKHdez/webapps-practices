0c9a1a062d99370091f265afce6a8802de2620e4
Victor Hdez <atlante_infinito@outlook.com>
2020-08-31 00:44:05 -0500

    ---> Count Sorting | MeanVariance Work

--- php/activity2/activity2.php
+++ php/activity2/activity2.php
@@ -25,6 +25,8 @@
                 $data->ordena();
             }
 
+            $data->medVar();
+
         }else{
             echo "<div class='alert'>N no es tamaño válido</div>";   
         } 
@@ -40,7 +42,40 @@
 
         <strong>Cadena:</strong> <br> <br>
 
-        <p> <?php $data->muestra(); ?> </p>
+        <p> Datos : <?php $data->muestra( $data->datos ); ?> </p> <br>
+        <?php
+            if(isset($_POST['sortvalue'])){
+                echo "<p> Datos Ordenados  : </p> ";
+                $data->muestra( $data->sorted );
+                echo "<br> <br> ";
+            }
+        ?>
+
+        <strong>Media:</strong> 
+        <p> <?php echo $data->media; ?> </p>
+
+        <strong>Varianza:</strong> 
+        <p> <?php echo $data->varianza; ?> </p> <br>
+
+        <strong>Cantidad de Valores:</strong> <br> <br>
+
+        <p> <?php
+            // Show the Quantity of the elements in the array
+
+            $arrayAux   = array( $data->max );
+            $countArray = $data->cuenta( $data->datos );
+
+
+            for($i=0; $i<=$data->max; $i++){
+                echo $i+1;
+                echo " --> [".$countArray[$i]."] . ";
+
+                if( $i>0 && $i%10 == 0){
+                    echo "<br> <br>";
+                }
+                
+            }
+        ?> </p>
     </div>
     
 </body>
--- php/activity2/activity2class.php
+++ php/activity2/activity2class.php
@@ -4,44 +4,116 @@ class Datos{
 
     // Attributes
     public $n;
-    public $datos =  array();
+    public $max;
+    public $datos  = array();
+    public $sorted = array();
+
+    public $media;
+    public $varianza;
 
     // CONSTRUCTOR
     public function __construct($size){
-        $this->n = $size;
-        $this->datos = array($size);
+        $this->n      = $size;
+        $this->datos  = array($size);
+
+        $this->max     = 0;
+        $this->media   = 0;
+        $this->varianza = 0;
     }
 
     // ------- MAIN Methods
 
     public function ordena(){
 
+        // Implementing Count Algorithm
+        $aux   = $this->cuenta($this->datos);
+        $final = array(); // Create Final array that contains Final index positions
+
+        for($i = 0; $i<$this->n; $i++){
+            $final[$i] = 0;
+        }
+
+        // Initiate SUM process
+        for($i = 2; $i<=$this->max; $i++){
+            $aux[$i] = $aux[$i] + $aux[ ($i-1) ];
+        }
+
+        // Set final indexing positions
+        
+        for($i=0; $i<$this->n; $i++){            
+            $final[ $aux[ $this->datos[$i] ]-1 ] = $this->datos[$i];
+            $aux[ $this->datos[$i]] --;
+        }
+
+        $this->sorted = $final;
     }
 
-    public function cuenta(){
+    public function cuenta($array){
 
+        // print_r($array);
+
+        $aux = array(); // Create the counting array.
+
+        for($i=0;$i<=$this->max; $i++){
+            $aux[$i] = 0;
+        }
+
+        // Count the array elements
+        for($i=0; $i<$this->n; $i++){
+            $aux[ $array[$i] ]++; // Increase the Count value for each array[i] element 
+        }
+        return $aux;
     }
 
     public function medVar(){
 
+        $this->media    = array_sum($this->datos) / $this->n;
+        $auxVarianza = 0.0;
+
+        foreach( $this->datos as $valor ){
+            $auxVarianza += pow($valor-$this->media, 2);
+        }
+
+        $auxVarianza /= $this->n;
+
+        $this->varianza = $auxVarianza;
     }
 
-    public function muestra(){
+    public function muestra($array){
 
-        for($i = 0;$i<$this->n;$i++){
-            echo  $this->datos[$i]." - ";
+        foreach($array as $value){
+            echo  $value." - ";
         }
     }
 
     // ------- Methods
+
     public function fillArray(){
 
         $i = 0;
 
         for( $i=0; $i < $this->n; $i++){
             $this->datos[$i] = rand(1,50);
+            $this->sorted[$i] = 0;
         }
+
+        $this->max = $this->higher($this->datos);
     }
+
+    public function higher($array){
+
+        $i = 0; $max = 0;
+
+        for( $i=0; $i < $this->n; $i++){
+
+            if($array[$i] > $max){
+                $max = $array[$i];
+            }
+        }
+
+        return $max;
+    }
+
 
 } // -----> END 'Datos' Class
 
